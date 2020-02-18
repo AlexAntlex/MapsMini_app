2fdbeee9ae34a0a939050bbe76a701399ccf125e
AlexAntlex <57757933+AlexAntlex@users.noreply.github.com>
2020-02-18 16:24:19 +0300

    Update map_find.py

--- map_find.py
+++ map_find.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import requests
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QInputDialog, QLineEdit, QPushButton
 
